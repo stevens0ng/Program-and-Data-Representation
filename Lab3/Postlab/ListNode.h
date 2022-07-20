45f4a4694a6cbc3457784db07ddda7afe8214781
Steven Song <89991942+stevens0ng@users.noreply.github.com>
2022-07-20 16:26:50 -0400

    Create ListNode.h

---
+++ Lab3/Postlab/ListNode.h
@@ -0,0 +1,48 @@
+/*
+ * Filename: ListNode.h
+ * Description: ListNode class definition
+ */
+
+/* Name: Steven Song
+   UVA ID: sqs5hrn 
+   Date: February 25, 2021  
+   CS2150 Lab 3 Post Lab ListNode.h 
+*/
+
+// NOTE: ListNode.h was copied over here from the Lab 2 Post Lab with one important new modification at the bottom of this file 
+
+// ListNode.h was downloaded from the source code/PDR (didn't modify this file)
+
+// A ListNode contains an integer value, as well as next and previous pointers to other ListNodes 
+
+#ifndef LISTNODE_H
+#define LISTNODE_H
+
+#include <iostream> // Necessary for cin/cout 
+
+// next line needed because NULL is part of std namespace
+using namespace std; // Defines the scope of the class 
+
+class ListNode {
+public:
+    ListNode();         // Constructor
+
+private:
+    int value;          // The value of the node!
+    ListNode* next;     // The next ListNode in the List
+    ListNode* previous; // The previous ListNode in the list
+    // (i.e. current -> next == NULL)
+    // (i.e. current -> previous == NULL)
+
+    // List needs access to value, next, and previous
+    friend class List;
+
+    // ListItr needs access to value
+    friend class ListItr;
+
+    // Not writing a destructor is fine in this case since there is no
+    // dynamically allocated memory in this class.
+    friend class Stack; // You need to include this for the PostLab in Lab 3
+}; // We have to include friend class Stack in ListNode.h 
+
+#endif
